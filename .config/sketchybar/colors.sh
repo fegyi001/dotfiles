0f5cf8017d943da2d4050e963243f60f8ce93430
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2024-01-12 20:48:41 +0100

    add sketchybar

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,8 @@
+export WHITE=0xffffffff
+
+# -- Gray Scheme --
+# export BAR_COLOR=0xaa101314
+export BAR_COLOR=0xff4B4B4A
+export ITEM_BG_COLOR=0x00353c3f
+export ACCENT_COLOR=0xffffffff
+export TEXT_COLOR=0xff101314
---
+++ .config/sketchybar/plugins/battery.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+source "$CONFIG_DIR/colors.sh" # Loads all defined colors
+
+PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(pmset -g batt | grep 'AC Power')
+
+if [ $PERCENTAGE = "" ]; then
+	exit 0
+fi
+
+case ${PERCENTAGE} in
+9[0-9] | 100)
+	ICON=""
+	;;
+[6-8][0-9])
+	ICON=""
+	;;
+[3-5][0-9])
+	ICON=""
+	;;
+[1-2][0-9])
+	ICON=""
+	;;
+*) ICON="" ;;
+esac
+
+if [[ $CHARGING != "" ]]; then
+	ICON=""
+fi
+
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
