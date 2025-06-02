0bf3cf1db83914379977d2417b897c229787c54a
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-06-02 10:52:42 +0200

    install script

--- install/cask.sh
+++ install/cask.sh
@@ -1,1 +1,1 @@
-brew install --cask dbeaver-community gimp obsidian font-sf-mono-nerd-font-ligaturized google-chrome wezterm keycastr
+brew install --cask dbeaver-community gimp obsidian font-sf-mono-nerd-font-ligaturized google-chrome wezterm keycastr font-jetbrains-mono-nerd-font font-jetbrains-mono
