85ddbe8848a166eed3a65c511ed93dc20a929ad5
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-08-29 21:11:04 +0200

    macros.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.icons")
-
--- CUSTOM MACROS
-
---Log variable in JS/TS
--- https://www.youtube.com/watch?v=Y3XWijJgdJs
-local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
-vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  group = "JSLogMacro",
-  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-  callback = function()
-    vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
-  end,
-})
-
-vim.api.nvim_create_augroup("RustLogMacro", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  group = "RustLogMacro",
-  pattern = { "rust" },
-  callback = function()
-    vim.fn.setreg("l", "yoprintln!(\"" .. esc .. "pa: {:?}\", " .. esc .. "pa);" .. esc)
-  end,
-})
+require("config.macros")
