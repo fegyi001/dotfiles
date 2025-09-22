fa2b554c5d55dd21b5f6cc766f06d2ef5a41361f
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-09-22 21:19:17 +0200

    add install-npm-packages.sh

---
+++ install/install-npm-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+npm install -g \
+  @angular/cli \
+  @angular/language-server \
+  @mermaid-js/mermaid-cli \
+  corepack \
+  prettier \
+  typescript \
+  typescript-language-server \
+  http-server \
+  yarn
