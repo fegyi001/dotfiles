1f5ebd42ea99cd2d81730a02eeee8f000813da77
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-12-15 20:10:46 +0100

    nvim: updates, tokyonight

--- .config/nvim/lua/plugins/colorscheme/tokyonight.lua
+++ .config/nvim/lua/plugins/colorscheme/tokyonight.lua
@@ -6,26 +6,14 @@ return {
     style = "moon",
     cache = true,
     dim_inactive = true,
-    -- transparent = true,
     styles = {
       comments = { italic = true },
       keywords = { italic = true },
       functions = { italic = true },
       variables = {},
-      -- sidebars = "transparent",
-      -- floats = "transparent",
     },
     on_colors = function(colors)
       colors.border = "#565f89"
-    end,
-    on_highlights = function(hl, colors)
-      -- hl.CursorLine = { bg = "NONE" }
-      -- hl.LspInlayHint = { bg = "NONE", fg = colors.dark3 }
-      -- hl.AerialLine = { bg = "NONE", fg = colors.dark3 }
-      -- hl.BufferLineSeparator = { fg = colors.bg, bg = colors.bg }
-      -- hl.BufferLineSeparatorSelected = { fg = colors.bg, bg = colors.bg }
-      -- hl.BufferLineSeparatorVisible = { fg = colors.bg, bg = colors.bg }
-      -- hl.StatusLine = { bg = "NONE" }
     end,
   },
 }
