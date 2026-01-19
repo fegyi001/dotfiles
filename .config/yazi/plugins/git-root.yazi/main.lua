a1541de1a768d07f6346e608e19be78aeb2ee274
Gergely Padányi-Gulyás <work@gpadanyig.com>
2026-01-19 13:59:43 +0100

    yazi: fix git root plugin

--- .config/yazi/plugins/git-root.yazi/main.lua
+++ .config/yazi/plugins/git-root.yazi/main.lua
@@ -1,14 +1,35 @@
--- Go to git root directory
-local function entry()
-	local handle = io.popen("git rev-parse --show-toplevel 2>/dev/null")
-	if handle then
-		local git_root = handle:read("*a"):gsub("%s+", "")
-		handle:close()
-		
-		if git_root and git_root ~= "" then
-			ya.manager_emit("cd", { git_root })
-		end
+--- @since 25.5.31
+
+local function get_git_toplevel()
+	local command = "git rev-parse --show-toplevel 2>&1"
+	local handle = io.popen(command)
+	local result = handle:read("*a")
+	local status_table = { handle:close() }
+	local status_code = status_table[3]
+
+	if status_code == 0 then
+		local destination = result:gsub("[\n\r]", "") .. "/"
+		return destination
+	else
+		return nil
 	end
 end
 
-return { entry = entry }
+return {
+	entry = function()
+		local destination = get_git_toplevel()
+		ya.dbg(destination)
+		if destination then
+			local target = Url(destination)
+			ya.emit("cd", { target })
+		else
+			ya.notify({
+				title = "Could not change directory!",
+				content = "You are not in a git repository.",
+				timeout = 3,
+				level = "error",
+			})
+		end
+	end,
+}
+
