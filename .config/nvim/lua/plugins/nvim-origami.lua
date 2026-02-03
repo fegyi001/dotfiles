ac5ea6f8b45600551e8ce445dfe38045b6b4991c
Gergely Padányi-Gulyás <work@gpadanyig.com>
2026-02-03 15:29:12 +0100

    nvim: updates, use again origami

--- .config/nvim/lua/plugins/nvim-origami.lua
+++ .config/nvim/lua/plugins/nvim-origami.lua
@@ -1,7 +1,33 @@
 return {
   "chrisgrieser/nvim-origami",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    useLspFoldsWithTreesitterFallback = {
+      enabled = true,
+      foldmethodIfNeitherIsAvailable = "indent",
+    },
+    pauseFoldsOnSearch = true,
+    foldtext = {
+      enabled = true,
+      padding = 3,
+      lineCount = {
+        template = "%d lines",
+        hlgroup = "Comment",
+      },
+      diagnosticsCount = true,
+      gitsignsCount = true,
+      disableOnFt = { "snacks_picker_input" },
+    },
+    autoFold = {
+      enabled = true,
+      kinds = { "imports" },
+    },
+    foldKeymaps = {
+      setup = true, -- modifies `h`, `l`, `^`, and `$`
+      closeOnlyOnFirstColumn = true, -- `h` and `^` only close in the 1st column
+      scrollLeftOnCaret = false, -- `^` should scroll left (basically mapped to `0^`)
+    },
+  },
   init = function()
     vim.opt.foldlevel = 99
     vim.opt.foldlevelstart = 99
