409d24fc016db7b9258be50c40925deabaaab69d
Gergely Padányi-Gulyás <work@gpadanyig.com>
2026-07-28 15:20:43 +0200

    wezterm: herdr workaround for Esc key

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,21 @@ local colorscheme = wezterm.color.load_scheme(os.getenv("HOME") .. "/.config/wez
 local backgroundColor = "#222437"
 -- local backgroundColor = "#1C1D2B"
 
+-- Preserve Cmd+Shift workspace navigation for Herdr without enabling the
+-- Kitty keyboard protocol globally, which currently breaks plain Escape.
+-- See https://github.com/sendhil/dotfiles-mac/commit/4f911efffd887b9e6b09d7797491e61944ab6c3f#diff-f022f8cd47d8636a83146650a2bbeb99130a8d3797431c8ebadfccf97585aa93
+-- https://github.com/ogulcancelik/herdr/issues/1266
+local function send_herdr_key(sequence, fallback_key, fallback_mods)
+	return wezterm.action_callback(function(window, pane)
+		local process = pane:get_foreground_process_name() or ""
+		if process:match("/herdr$") then
+			pane:send_text(sequence)
+		else
+			window:perform_action(wezterm.action.SendKey({ key = fallback_key, mods = fallback_mods }), pane)
+		end
+	end)
+end
+
 config = {
 	color_schemes = {
 		["tokyonight_moon"] = colorscheme,
@@ -108,6 +123,26 @@ config = {
 		{ key = "-", mods = "CMD", action = wezterm.action.DecreaseFontSize },
 		{ key = "+", mods = "CMD", action = wezterm.action.IncreaseFontSize },
 		{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+		{
+			key = "Escape",
+			mods = "NONE",
+			action = send_herdr_key("\x1b[27;1u", "Escape", "NONE"),
+		},
+		{
+			key = "UpArrow",
+			mods = "CMD|SHIFT",
+			action = send_herdr_key("\x1b[1;10A", "UpArrow", "CMD|SHIFT"),
+		},
+		{
+			key = "DownArrow",
+			mods = "CMD|SHIFT",
+			action = send_herdr_key("\x1b[1;10B", "DownArrow", "CMD|SHIFT"),
+		},
+		{
+			key = "r",
+			mods = "CMD|SHIFT",
+			action = send_herdr_key("\x1b[114:82;10u", "r", "CMD|SHIFT"),
+		},
 	},
 }
 
