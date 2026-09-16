377feb25b00bcc009b37581781c50bc042853119
Gergely Padányi-Gulyás <work@gpadanyig.com>
2026-09-16 21:29:28 +0200

    hypr: fix bindings

--- .config/hypr/bindings.lua
+++ .config/hypr/bindings.lua
@@ -31,27 +31,28 @@ hl.unbind("SUPER, PRINT")
 o.bind("SUPER + SHIFT + C", "Color picker", "pkill hyprpicker || hyprpicker -a")
 
 -- Launch apps
-hl.unbind("SUPER, SPACE")
-o.bind("SUPER + SPACE", "Omarchy menu", "omarchy-menu")
-hl.unbind("SUPER + ALT, SPACE")
+--hl.unbind("SUPER, SPACE")
+--o.bind("SUPER + SPACE", "Omarchy menu", "omarchy-menu")
+hl.unbind("SUPER + ALT + SPACE")
 o.bind("SUPER + M", "Omarchy menu", "omarchy-menu")
 
 -- TILING
-hl.unbind("SUPER, W")
-o.bind("SUPER + Q", "Close window", "killactive")
-hl.unbind("CTRL + ALT, DELETE")
+hl.unbind("SUPER + W")
+o.bind("SUPER + Q", "Close window", hl.dsp.window.close())
+hl.unbind("CTRL + ALT + DELETE")
 o.bind("SUPER + SHIFT + Q", "Close all windows", "omarchy-hyprland-window-close-all")
 
 -- Nvim-style window focus movement
-hl.unbind("SUPER, K")
-hl.unbind("SUPER, SLASH")
-hl.unbind("SUPER, H")
-hl.unbind("SUPER, L")
-hl.unbind("SUPER, J")
-o.bind("SUPER + H", "Move window focus left", "movefocus l")
-o.bind("SUPER + L", "Move window focus right", "movefocus r")
-o.bind("SUPER + K", "Move window focus up", "movefocus u")
-o.bind("SUPER + J", "Move window focus down", "movefocus d")
+hl.unbind("SUPER + K")
+hl.unbind("SUPER + SLASH")
+hl.unbind("SUPER + H")
+hl.unbind("SUPER + L")
+hl.unbind("SUPER + J")
+
+o.bind("SUPER + H", "Focus on left window", hl.dsp.focus({ direction = "l" }))
+o.bind("SUPER + L", "Focus on right window", hl.dsp.focus({ direction = "r" }))
+o.bind("SUPER + K", "Focus on above window", hl.dsp.focus({ direction = "u" }))
+o.bind("SUPER + J", "Focus on below window", hl.dsp.focus({ direction = "d" }))
 o.bind("SUPER + SLASH", "Show key bindings", "omarchy-menu-keybindings")
 
 -- Switch apps
@@ -59,7 +60,7 @@ o.bind("SUPER + CTRL + ALT + J", "Firefox", 'omarchy-launch-or-focus firefox "uw
 o.bind("SUPER + CTRL + ALT + SHIFT + J", "Firefox", 'omarchy-launch-or-focus firefox "uwsm-app -- firefox"')
 o.bind("SUPER + CTRL + ALT + I", "Chromium", 'omarchy-launch-or-focus chromium "uwsm-app -- chromium"')
 o.bind("SUPER + CTRL + ALT + SHIFT + I", "Chromium", 'omarchy-launch-or-focus chromium "uwsm-app -- chromium"')
-o.bind("SUPER + CTRL + ALT + K", "Foot", 'omarchy-launch-or-focus foot "uwsm-app -- alacritty"')
+o.bind("SUPER + CTRL + ALT + K", "Foot", 'omarchy-launch-or-focus foot "uwsm-app -- foot"')
 o.bind("SUPER + CTRL + ALT + SHIFT + K", "Foot", 'omarchy-launch-or-focus foot "uwsm-app -- alacritty"')
 o.bind("SUPER + CTRL + ALT + M", "Microsoft Teams", 'omarchy-launch-or-focus teams "uwsm-app -- teams-for-linux"')
 o.bind(
@@ -74,9 +75,9 @@ o.bind("SUPER + SHIFT + F11", "Screenshot Region", "omarchy-cmd-screenshot")
 o.bind("SUPER + SHIFT + F12", "Screenshot Display", "omarchy-cmd-screenshot output")
 
 -- Volume
-hl.unbind("SUPER, F10")
-hl.unbind("SUPER, F11")
-hl.unbind("SUPER, F12")
+hl.unbind("SUPER + F10")
+hl.unbind("SUPER + F11")
+hl.unbind("SUPER + F12")
 o.bind("SUPER + F10", "Mute", "$osdclient --output-volume mute-toggle")
 o.bind("SUPER + F11", "Volume down", "$osdclient --output-volume lower")
 o.bind("SUPER + F12", "Volume up", "$osdclient --output-volume raise")
--- .config/hypr/input.lua
+++ .config/hypr/input.lua
@@ -4,42 +4,42 @@
 -- Keyboard layout and options.
 -- See https://wiki.hypr.land/Configuring/Basics/Variables/#input
 hl.config({
-  input = {
-    -- Use multiple keyboard layouts and switch between them with Alt(Option)+Shift
-    kb_layout = "us,hu",
-    kb_variant = ",qwertz_keyd",
-    kb_options = "compose:caps,grp:alt_shift_toggle",
+	input = {
+		-- Use multiple keyboard layouts and switch between them with Alt(Option)+Shift
+		kb_layout = "us,hu",
+		kb_variant = ",qwertz_keyd",
+		kb_options = "compose:caps,grp:alt_shift_toggle",
 
-    -- Change speed of keyboard repeat.
-    repeat_rate = 40,
-    repeat_delay = 250,
+		-- Change speed of keyboard repeat.
+		repeat_rate = 40,
+		repeat_delay = 250,
 
-    -- Start with numlock on by default.
-    numlock_by_default = true,
+		-- Start with numlock on by default.
+		numlock_by_default = true,
 
-    -- Increase sensitivity for mouse/trackpad (default: 0).
-    sensitivity = 0.35,
+		-- Increase sensitivity for mouse/trackpad (default: 0).
+		sensitivity = 0.35,
 
-    -- Turn off mouse acceleration (default: adaptive).
-    accel_profile = "flat",
+		-- Turn off mouse acceleration (default: adaptive).
+		accel_profile = "flat",
 
-    touchpad = {
-      -- Use natural (inverse) scrolling.
-      natural_scroll = true,
+		touchpad = {
+			-- Use natural (inverse) scrolling.
+			natural_scroll = true,
 
-      -- Use two-finger clicks for right-click instead of lower-right corner.
-      clickfinger_behavior = true,
+			-- Use two-finger clicks for right-click instead of lower-right corner.
+			clickfinger_behavior = true,
 
-      -- Control the speed of your scrolling.
-      scroll_factor = 0.4,
+			-- Control the speed of your scrolling.
+			scroll_factor = 0.4,
 
-      -- Enable the touchpad while typing.
-      disable_while_typing = false,
+			-- Enable the touchpad while typing.
+			disable_while_typing = false,
 
-      -- Left-click-and-drag with three fingers.
-      drag_3fg = 1,
-    },
-  },
+			-- Left-click-and-drag with three fingers.
+			drag_3fg = 1,
+		},
+	},
 })
 
 -- App-specific touchpad scroll speeds.
