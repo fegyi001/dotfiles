a00f357c25a0562981f35b81c024bd3afd6e88ef
Gergely Padányi-Gulyás <work@gpadanyig.com>
2026-09-23 15:28:01 +0200

    omarchy: rounding, monitors

--- .config/hypr/looknfeel.lua
+++ .config/hypr/looknfeel.lua
@@ -28,7 +28,6 @@ hl.config({
 	decoration = {
 		-- Use round window corners.
 		rounding = 4,
-
 		-- Dim unfocused windows (0.0 = no dim, 1.0 = fully dimmed).
 		-- dim_inactive = true,
 		-- dim_strength = 0.15,
